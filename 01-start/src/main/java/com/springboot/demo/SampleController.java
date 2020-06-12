7493fecc3b962b4357f78818605b8599217f1432
yinqs <1119334540@qq.com>
2020-06-12 19:14:52 +0800

    test

---
+++ 01-start/src/main/java/com/springboot/demo/SampleController.java
@@ -0,0 +1,13 @@
+package com.springboot.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author yinqs
+ */
+@Controller
+public class SampleController {
+
+
+}
